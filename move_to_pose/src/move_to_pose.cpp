ffde0b522ae1ffab4daf9289d81f321245da388b
Ayush <ayush00414@gmail.com>
2026-02-25 19:10:19 -0500

    fix: enable collision checking and identify table-top planning issues
    Collisions are working, but the current planner is unable to resolve paths with the table present and produces unnatural trajectories. Looking into alternative planners.

--- move_to_pose/src/move_to_pose.cpp
+++ move_to_pose/src/move_to_pose.cpp
@@ -108,10 +108,10 @@ int main(int argc, char * argv[])
   std::vector<moveit_msgs::msg::CollisionObject> collision_objects;
 
   // --- scene.xml: table (body pos="0 0 -0.584", geom size="0.343 0.546 0.584") ---
-  collision_objects.push_back(make_box(
-    "table", frame,
-    {0.0, 0.0, -1.0},
-    {0.343, 0.546, 0.584}));
+  // collision_objects.push_back(make_box(
+  //   "table", frame,
+  //   {0.0, 0.0, -0.6},
+  //   {0.343, 0.546, 0.584}));
 
   // --- scene.xml: frame left_post (pos="0 -0.597 0.5715", size="0.02 0.02 0.5715") ---
   collision_objects.push_back(make_box(
@@ -119,23 +119,23 @@ int main(int argc, char * argv[])
     {0.0, -0.597, 0.5715},
     {0.02, 0.02, 0.5715}));
 
-  // --- scene.xml: frame right_post (pos="0 0.597 0.5715", size="0.02 0.02 0.5715") ---
-  // collision_objects.push_back(make_box(
-  //   "right_post", frame,
-  //   {0.0, 0.597, 0.5715},
-  //   {0.02, 0.02, 0.5715}));
+  --- scene.xml: frame right_post (pos="0 0.597 0.5715", size="0.02 0.02 0.5715") ---
+  collision_objects.push_back(make_box(
+    "right_post", frame,
+    {0.0, 0.597, 0.5715},
+    {0.02, 0.02, 0.5715}));
 
-  // --- scene.xml: frame crossbar (pos="0 0 1.143", size="0.02 0.597 0.02") ---
-  // collision_objects.push_back(make_box(
-  //   "crossbar", frame,
-  //   {0.0, 0.0, 1.143},
-  //   {0.02, 0.597, 0.02}));
+  --- scene.xml: frame crossbar (pos="0 0 1.143", size="0.02 0.597 0.02") ---
+  collision_objects.push_back(make_box(
+    "crossbar", frame,
+    {0.0, 0.0, 1.143},
+    {0.02, 0.597, 0.02}));
 
-  // --- box.xml: obj_box_01 (pos="-0.3 0.3 0.03", size="0.03 0.03 0.03") ---
-  // collision_objects.push_back(make_box(
-  //   "obj_box_01", frame,
-  //   {-0.3, 0.3, 0.03},
-  //   {0.03, 0.03, 0.03}));
+  --- box.xml: obj_box_01 (pos="-0.3 0.3 0.03", size="0.03 0.03 0.03") ---
+  collision_objects.push_back(make_box(
+    "obj_box_01", frame,
+    {-0.3, 0.3, 0.03},
+    {0.03, 0.03, 0.03}));
 
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
   planning_scene_interface.applyCollisionObjects(collision_objects);
